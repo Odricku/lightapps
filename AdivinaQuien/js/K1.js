e3f1eab1840b014f06860cc0020f85008eb60dbb
Odricku <pedro.rojasb@usach.cl>
2023-02-20 19:25:01 -0300

    change imgs 2

--- AdivinaQuien/js/K1.js
+++ AdivinaQuien/js/K1.js
@@ -1115,7 +1115,7 @@ function cargahabs(){
 			for(var i = 0; i < habsarray.length; i++){
 				
 				pos1 = habsarray[i].indexOf("title=") + 7;
-				pos2 = habsarray[i].indexOf("\">");
+				pos2 = habsarray[i].indexOf("\">", pos1);
 				
 				var nombrehab = habsarray[i].substring(pos1, pos2);
 				
