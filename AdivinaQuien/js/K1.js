bb8af864792be22f98de3d219b34e52fb11b9e98
Odricku <pedro.rojasb@usach.cl>
2023-02-22 02:03:18 -0300

    auto-commit

--- AdivinaQuien/js/K1.js
+++ AdivinaQuien/js/K1.js
@@ -1304,29 +1304,29 @@ function cargapoke(id, variacion){
 			
 			//Stats
 			
-			var posstats = 
+			var posstats = pokeinfo.indexOf("title=\"Características\">características</a>");
 			
-			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">PS</a>", posfin)) + 3) + 1;
+			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">PS</a>", posstats)) + 3) + 1;
 			posfin = pokeinfo.indexOf("<", posini);
 			pokelist2[id][variacion].stats[0] = parseInt(pokeinfo.substring(posini, posfin));
 			
-			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Ataque</a>", posfin)) + 3) + 1;
+			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Ataque</a>", posstats)) + 3) + 1;
 			posfin = pokeinfo.indexOf("<", posini);
 			pokelist2[id][variacion].stats[1] = parseInt(pokeinfo.substring(posini, posfin));
 			
-			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Defensa</a>", posfin)) + 3) + 1;
+			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Defensa</a>", posstats)) + 3) + 1;
 			posfin = pokeinfo.indexOf("<", posini);
 			pokelist2[id][variacion].stats[2] = parseInt(pokeinfo.substring(posini, posfin));
 			
-			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">At. esp.</a>", posfin)) + 3) + 1;
+			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">At. esp.</a>", posstats)) + 3) + 1;
 			posfin = pokeinfo.indexOf("<", posini);
 			pokelist2[id][variacion].stats[3] = parseInt(pokeinfo.substring(posini, posfin));
 			
-			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Def. esp.</a>", posfin)) + 3) + 1;
+			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Def. esp.</a>", posstats)) + 3) + 1;
 			posfin = pokeinfo.indexOf("<", posini);
 			pokelist2[id][variacion].stats[4] = parseInt(pokeinfo.substring(posini, posfin));
 
-			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Velocidad</a>", posfin)) + 3) + 1;
+			posini = pokeinfo.indexOf(">", pokeinfo.indexOf("<td", pokeinfo.indexOf(">Velocidad</a>", posstats)) + 3) + 1;
 			posfin = pokeinfo.indexOf("<", posini);
 			pokelist2[id][variacion].stats[5] = parseInt(pokeinfo.substring(posini, posfin));
 			
