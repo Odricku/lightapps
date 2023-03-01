02f47d7ffbdc5d146fbe8eb583e21c9df1a50ed5
Odricku <pedro.rojasb@usach.cl>
2023-03-01 06:17:23 -0300

    auto-commit

--- dice/d1.js
+++ dice/d1.js
@@ -1,26 +1,27 @@
 function rollDice() {
-  const dice = [...document.querySelectorAll(".die-list")];
-  dice.forEach(die => {
-    toggleClasses(die);
-    die.dataset.roll = getRandomNumber(1, 6);
-  });
+	const dice = [...document.querySelectorAll(".die-list")];
+	dice.forEach(die => {
+		toggleClasses(die);
+		die.dataset.roll = getRandomNumber(1, 6);
+	});
+	editdice.style.display = "none";
 }
 
 function toggleClasses(die) {
-  die.classList.toggle("odd-roll");
-  die.classList.toggle("even-roll");
+	die.classList.toggle("odd-roll");
+	die.classList.toggle("even-roll");
 }
 
 function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
   
 }
 
 document.getElementById("roll-button").addEventListener("click", rollDice);
 
-var cantDado = 0;
+var cantDado = 1;
 var dicefocus;
 
 function addDado(){
@@ -51,10 +52,7 @@ function xPuntitos(num){
 function changeName(node){
 
 	dicefocus = node;
-	
-	backeditdice.style.display = "block";
 	editdice.style.display = "block";
-	
 	nombreDado.value = node.nextElementSibling.innerHTML;
 
 }
@@ -73,6 +71,5 @@ function changesbutton(){
 }
 
 function ocultarEdit(){
-	backeditdice.style.display = "none";
 	editdice.style.display = "none";
 }
